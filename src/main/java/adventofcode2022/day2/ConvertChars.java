b474e3abd03dd4aba120439b78b7b2ad0672d769
catherine.wates <catherine.wates@autotrader.co.uk>
2024-09-19 11:22:03 +0100

    Day 2 REFACTOR - Refactoring ConvertChars to use a map

--- src/main/java/adventofcode2022/day2/ConvertChars.java
+++ src/main/java/adventofcode2022/day2/ConvertChars.java
@@ -1,22 +1,39 @@
 package adventofcode2022.day2;
 
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class ConvertChars {
 
+    Map<String, String> characterConversion = new HashMap<>();
+
+//    Alternative approach:
+//    Map<String, Integer> testMap = Map.of(
+//            "A", 1,
+//            "B", 2
+//    );
+
+    public ConvertChars() {
+        characterConversion.put("A", "1");
+        characterConversion.put("B", "2");
+        characterConversion.put("C", "3");
+        characterConversion.put("X", "1");
+        characterConversion.put("Y", "2");
+        characterConversion.put("Z", "3");
+    }
+
     public ArrayList<String> convertCharsToInts(ArrayList<String> strategyPerRound) {
         ArrayList<String> convertedStrategyPerRound = new ArrayList<>();
+        Set<String> keys = characterConversion.keySet();
         for (String strategy : strategyPerRound) {
-            String conversion = strategy.replace("A", "1")
-                    .replace("B", "2")
-                    .replace("C", "3")
-                    .replace("X", "1")
-                    .replace("Y", "2")
-                    .replace("Z", "3");
-            convertedStrategyPerRound.add(conversion);
+            String conversion;
+            for (String key : keys) {
+                if (Objects.equals(strategy, key)) {
+                    conversion = strategy.replace(key, characterConversion.get(key));
+                    convertedStrategyPerRound.add(conversion);
+                }
+            }
         }
-
         return convertedStrategyPerRound;
     }
 }
